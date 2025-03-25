73c112c438c406e5aed0ae1fcb8ab2d2ba2ae5c0
Lore <khasanboykupalov1@gmail.com>
2025-03-25 23:37:20 +0500

    created interface

---
+++ src/app/pagination/interface.ts
@@ -0,0 +1,28 @@
+export interface ApiResponse{
+    page: number;
+    pageSize: number;
+    count: number;
+    total: number;
+    pagesCount: number
+    data: Problem[]; 
+}
+
+
+export interface Problem {
+    id: number;
+    title: string;
+    difficulty: number;
+    difficultyTitle: string;
+    solved: number;
+    userInfo:{ hasSolved: boolean; hasAttempted: boolean; };
+    authorUsername: string;
+    notSolved: number;
+    attemptsCount: number;
+    hasChecker: true;
+    tags: {id: number; name: string} [];
+    hasSolution: boolean;
+    solutionKepcoinValue: number;
+    likesCount: number;
+    dislikesCount: number;
+    hidden: boolean
+}
